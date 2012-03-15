8369dc6bcc5240c3d662915dd5114bfe956cde67
Simon Pomeroy <elscp@Simon-Pomeroys-MacBook-Pro.local>
2012-03-15 18:52:24 +0000

    Initial Commit

---
+++ Whereami/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Whereami
+//
+//  Created by Simon Pomeroy on 15/03/2012.
+//  Copyright (c) 2012 Loughborough University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Whereami/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Whereami
+//
+//  Created by Simon Pomeroy on 15/03/2012.
+//  Copyright (c) 2012 Loughborough University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
